812be4d135d99668cfd2419e4456e1b304a6412a
Sam Hunter <shunter@gmail.com>
2020-03-30 09:41:26 -0700

    improved mapping

--- 02-map_reads.py
+++ 02-map_reads.py
@@ -4,6 +4,7 @@ import re
 
 outf = open("02-map_reads.sh", 'w')
 os.system('mkdir -p 02-mapped')
+os.system('mkdir -p 03-sorted')
 ref = '/share/biocore/projects/Ralston_K_UCD/sh-analysis/Reference/AmoebaDB-46_EhistolyticaHM1IMSS_Genome.fasta'
 
 
@@ -11,8 +12,16 @@ for r1 in glob("./01-cleaned/*_R1.fastq.gz"):
     r2 = r1.replace("_R1", "_R2")
     s = re.sub("_R1.*.gz$", '', r1.strip().split('/')[2])
     outf.write("echo " + s + '\n')
-    cmd = "bwa mem -t 110 -R '@RG\\tID:%s\\tSM:%s\\tPL:ILLUMINA\\tDS:Paired' %s %s %s |"% (s, s, ref, r1, r2)
+    cmd = "bwa mem -t 110 -M -R '@RG\\tID:%s\\tSM:%s\\tPL:ILLUMINA\\tDS:Paired' %s %s %s |"% (s, s, ref, r1, r2)
     cmd += " samtools view -o ./02-mapped/%s.bam -" % s
+    outf.write(cmd + '\n')
+    cmd = "samtools sort --threads 110 ./02-mapped/%s.bam -o ./03-sorted/%s_sorted.bam" % (s, s)
+    outf.write(cmd + '\n')
+    cmd = "samtools idxstats ./03-sorted/%s_sorted.bam > ./03-sorted/%s_sorted.idxstats"
+    outf.write(cmd + '\n')
+    cmd = "samtools flagstat ./03-sorted/%s_sorted.bam > ./03-sorted/%s_sorted.flagstat"
+    outf.write(cmd + '\n')
+    cmd = "samtools stats ./03-sorted/%s_sorted.bam > ./03-sorted/%s_sorted.stats"
     outf.write(cmd + '\n')
 
 outf.close()
