0e388d07ea1a08c676592fe066e7de1f19a43aac
Sam Hunter <shunter@gmail.com>
2020-03-30 09:07:13 -0700

    map

---
+++ 02-map_reads.py
@@ -0,0 +1,41 @@
+import os
+from glob import glob
+import re
+
+outf = open("02-map_reads.sh", 'w')
+os.system('mkdir -p 02-mapped')
+ref = '/share/biocore/projects/Ralston_K_UCD/sh-analysis/Reference/AmoebaDB-46_EhistolyticaHM1IMSS_Genome.fasta'
+
+
+for r1 in glob("./01-cleaned/*_R1.fastq.gz"):
+    r2 = r1.replace("_R1", "_R2")
+    s = re.sub("_R1.*.gz$", '', r1.strip().split('/')[2])
+    outf.write("echo " + s + '\n')
+    cmd = "bwa mem -t 110 -R '@RG\\tID:%s\\tSM:%s\\tPL:ILLUMINA\\tDS:Paired' %s %s %s |"% (s, s, ref, r1, r2)
+    cmd += " samtools view -o ./02-mapped/%s.bam -" % s
+    outf.write(cmd + '\n')
+
+outf.close()
+
+
+# call="bwa mem -t ${MAPTHREADS} \
+#   -R '@RG\tID:${sample}\tSM:${sample}\tPL:ILLUMINA\tDS:Paired' \
+#   ${mapfasta} ${se} | \
+#   samtools sort -m 768M --threads ${SORTTHREADS} -o ${output}-se -"
+
+
+# mapfasta=./Reference/AmoebaDB-46_EhistolyticaHM1IMSS_Genome.fasta
+
+# call="samtools index -@ ${THREADS} ${output}"
+# echo $call
+# eval $call
+
+# call="samtools idxstats ${output} > ${output}.idxstats"
+# echo $call
+# eval $call
+
+# call="samtools flagstat -@ ${THREADS} ${output} > ${output}.flagstat"
+# echo $call
+# eval $call
+
+# call="samtools stats -@ ${THREADS} ${output} > ${output}.stats"
